71a751462ab05e02ecc860109a94eddcdc4540af
WuTob <wutob12450@outlook.com>
2018-01-09 13:39:07 +0800

    RQNOJ

---
+++ RQNOJ/明明的随机数.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+void quickSort(int *a, int l, int r);
+void swap(int &a, int &b);
+
+int main()
+{
+    int a[] = {1, 52, 63, 12, 2, 3};
+
+    quickSort(a, 0, 5);
+
+    for(int i = 0; i < 6; i++)
+    {
+        cout << a[i];
+    }
+}
+
+void quickSort(int *a, int l, int r)
+{
+    if(l > r) return;
+
+    int stdNum = a[l];
+    int lhs = l, rhs = r;
+
+    while(lhs < rhs)
+    {
+        while(a[rhs] > stdNum && lhs < rhs) rhs--;
+        while(a[lhs] < stdNum && lhs < rhs) lhs++;
+    }
+
+    if(lhs != rhs)(swap(a[lhs], a[rhs]));
+    quickSort(l, lhs - 1);
+    quickSort(lhs + 1, r);
+}
+
+void swap(int &a, int &b)
+{
+    a = a ^ b;
+    b = b ^ a;
+    a = a ^ b;
+}
