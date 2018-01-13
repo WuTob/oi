11f83edec5bf4a3ecdd772671cb84d063810af94
dfdtc7 <34589240+dfdtc7@users.noreply.github.com>
2018-01-13 11:07:32 +0800

    update

--- 洛谷/P1002 过河卒.cpp
+++ 洛谷/P1002 过河卒.cpp
@@ -11,7 +11,7 @@ int main()
     POS soilder, horse, horseMove[9];
     cin >> soilder.x >> soilder.y >> horse.x >> horse.y;
 
-    
+    horseMove[0] = {horse.x + 1, horse.y + 2};
 
     return 0;
 }
