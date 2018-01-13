83f1259ddcb131ed8e63b818f8cf28bb34b40a99
dfdtc7 <34589240+dfdtc7@users.noreply.github.com>
2018-01-13 10:51:53 +0800

    update

---
+++ 洛谷/P1002 过河卒.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    struct POS{
+        int x;
+        int y;
+    };
+
+    POS soilder, horse, horseMove[9];
+    cin >> soilder.x >> soilder.y >> horse.x >> horse.y;
+
+    
+
+    return 0;
+}
