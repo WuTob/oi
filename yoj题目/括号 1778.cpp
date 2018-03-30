5878e88d730246d93c031bd2b1a91831ef7bfd9c
WuTob <wutob12450@outlook.com>
2018-03-30 14:19:34 +0800

    Create 括号 1778.cpp
    update

---
+++ yoj题目/括号 1778.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+void isOK(string s);
+string ss;
+
+int main()
+{
+    int n = 0;
+    cin >> n;
+
+    for(int i = 1; i < n; i++)
+    {
+        string s;
+        cin >> s;
+        isOK(s);
+        ss.append("\n");
+    }
+
+    string s;
+    cin >> s;
+    isOK(s);
+
+    cout << ss;
+
+    return 0;
+}
+
+void isOK(string s)
+{
+    stack<int> left;
+    static const char leftChar[4]  = {'(', '[', '<', '{'};
+    static const char rightChar[4] = {')', ']', '>', '}'};
+    bool flag = false;
+
+    for(int i = 0; i < s.size(); i++)
+    {
+        bool continueFlag = false;
+
+        for(int n = 0; n < 4; n++)
+        {
+            if(s[i] == leftChar[n]){
+                left.push(n);
+                continueFlag = true;
+            }
+        }
+        if(continueFlag) continue;
+
+        for(int n = 0; n < 4; n++)
+        {
+            if(s[i] == rightChar[n]){
+                if(left.empty() || n != left.top()){
+                    ss.append("NO");
+                    flag = true;
+                }
+                else    left.pop();
+            }
+        }
+        if(flag) break;
+    }
+
+    if(left.empty() && !flag)   ss.append("YES");
+}
